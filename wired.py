771234910f075f7cd37a36fb6336c7aa8928ab55
Ivan Aristy <ivanaristy@Ivan-Aristys-Macbook.local>
2020-11-12 19:10:58 -0500

    commenting out py file

--- wired.py
+++ wired.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template
 import feedparser
 import random
 import time
-from datetime import datetime, timezone
-from datetime import timedelta
+from datetime import datetime, timezone, timedelta
 import pandas as pd
 import os
 
@@ -14,25 +13,26 @@ app = Flask(__name__, template_folder=template_path)
 
 @app.route('/recents')
 
-def recents():
 
-    url = 'https://www.wired.com/feed/rss'
 
-    listobject = []
+def rng():
+    
+    #url = 'https://www.wired.com/feed/rss'
 
-    parsed = feedparser.parse(url)
+    #listobject = []
+
+    #parsed = feedparser.parse(url)
     #makes now equal to the date and time at which the rss feed was last updated
-    now = datetime.strptime(parsed['channel']['updated'], '%a, %d %b %Y %H:%M:%S +0000')
+    #now = datetime.strptime(parsed['channel']['updated'], '%a, %d %b %Y %H:%M:%S +0000')
 
     #iterating through the items
-    for feed in parsed.entries:
+    #for feed in parsed.entries:
         # setting colors on python not html
-        setRed = False
-        publishedDate = datetime.strptime(feed.published, '%a, %d %b %Y %H:%M:%S +0000')
-        difference = now - publishedDate
-        if difference.seconds < 3600 and difference.days == 0:
-            setRed = True
-        #passing and apending objects for html file
-        listobject.append({'title':feed.title,'description':feed.description,'published':feed.published, "red":setRed})
-    
-    return render_template('wired.html', listobject=listobject, now=now)
+    #    setRed = False
+    #    publishedDate = datetime.strptime(feed.published, '%a, %d %b %Y %H:%M:%S +0000')
+    #    difference = now - publishedDate
+    #    if difference.seconds < 3600 and difference.days == 0:
+    #        setRed = True
+    #    #passing and apending objects for html file
+    #    listobject.append({'title':feed.title,'description':feed.description,'published':feed.published, "red":setRed})
+    return 7
