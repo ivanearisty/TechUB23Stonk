7ea5d7cb19176fe8d3156f3bd22edab6d8e1c673
jhorowitz11 <74744403+jhorowitz11@users.noreply.github.com>
2020-11-24 18:53:08 -0500

    Update wired1.py

--- wired1.py
+++ wired1.py
@@ -94,145 +94,145 @@ def percent_change_margin_new(ticker, stock_return, margin):
     return a
 
 
-# # greater than a certain percent 
-# @app.route('/CheckPercentGreater/<ticker>/<float:percent>')# works
-# def check_percent_greater(ticker, percent):
-#     df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
-#         ticker, outputsize='full')[0]
-#     df["returns"] = df["5. adjusted close"].pct_change()*100
-#     df_sub = df.loc[(df['returns']) >= percent, :]
-#     df_sub = df_sub[["returns"]].sort_index(ascending=False)
-#     df_sub = df_sub.to_dict()
-#     final_dict = df_sub['returns']
-#     a = {}
-#     for i in final_dict:
-#         a[str(i)] = final_dict[i]
-#     return a
+# greater than a certain percent 
+@app.route('/CheckPercentGreater/<ticker>/<float:percent>')# works
+def check_percent_greater(ticker, percent):
+    df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
+        ticker, outputsize='full')[0]
+    df["returns"] = df["5. adjusted close"].pct_change()*100
+    df_sub = df.loc[(df['returns']) >= percent, :]
+    df_sub = df_sub[["returns"]].sort_index(ascending=False)
+    df_sub = df_sub.to_dict()
+    final_dict = df_sub['returns']
+    a = {}
+    for i in final_dict:
+        a[str(i)] = final_dict[i]
+    return a
     
 
-# # greater than a certain percent within a timeframe
-# @app.route('/CheckPercentGreaterTimeframe/<ticker>/<float:percent>/<start_date>/<end_date>') # works
-# def check_percent_greater_by_date(ticker, percent, start_date, end_date):
-#     assert pd.to_datetime(start_date) < pd.to_datetime(end_date)
-#     df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
-#         ticker, outputsize='full')[0]
-#     df = df.sort_index()
-#     df["returns"] = df["5. adjusted close"].pct_change()*100
-#     df_sub = df.loc[start_date:end_date]
-#     df_sub = df_sub.loc[(df['returns']) >= percent, :]
-#     df_sub = df_sub[["returns"]].sort_index(ascending=False).to_dict()
-#     final_dict = df_sub['returns']
-#     a = {}
-#     for i in final_dict:
-#         a[str(i)] = final_dict[i]
-#     return a
+# greater than a certain percent within a timeframe
+@app.route('/CheckPercentGreaterTimeframe/<ticker>/<float:percent>/<start_date>/<end_date>') # works
+def check_percent_greater_by_date(ticker, percent, start_date, end_date):
+    assert pd.to_datetime(start_date) < pd.to_datetime(end_date)
+    df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
+        ticker, outputsize='full')[0]
+    df = df.sort_index()
+    df["returns"] = df["5. adjusted close"].pct_change()*100
+    df_sub = df.loc[start_date:end_date]
+    df_sub = df_sub.loc[(df['returns']) >= percent, :]
+    df_sub = df_sub[["returns"]].sort_index(ascending=False).to_dict()
+    final_dict = df_sub['returns']
+    a = {}
+    for i in final_dict:
+        a[str(i)] = final_dict[i]
+    return a
 
-# # greater than a certain percent within a timeframe and period n 
-# @app.route('/CheckPercentGreaterTimeframePeriod/<ticker>/<int:n>/<float:percent>/<start_date>/<end_date>') # works
-# def check_percent_greater_by_date_timeframe(ticker, n, percent, start_date, end_date):
-#     assert pd.to_datetime(start_date) < pd.to_datetime(end_date)
-#     df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
-#         ticker, outputsize='full')[0]
-#     df = df.sort_index()
-#     df["returns"] = df["5. adjusted close"].pct_change(periods=n)*100
-#     df_sub = df.loc[start_date:end_date]
-#     df_sub = df_sub.loc[(df['returns']) >= percent, :]
-#     # return df_sub[["returns"]].sort_index()
-#     df_sub = df_sub[["returns"]].sort_index(ascending=False).to_dict()
-#     final_dict = df_sub['returns']
-#     a = {}
-#     for i in final_dict:
-#         a[str(i)] = final_dict[i]
-#     return a
+# greater than a certain percent within a timeframe and period n 
+@app.route('/CheckPercentGreaterTimeframePeriod/<ticker>/<int:n>/<float:percent>/<start_date>/<end_date>') # works
+def check_percent_greater_by_date_timeframe(ticker, n, percent, start_date, end_date):
+    assert pd.to_datetime(start_date) < pd.to_datetime(end_date)
+    df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
+        ticker, outputsize='full')[0]
+    df = df.sort_index()
+    df["returns"] = df["5. adjusted close"].pct_change(periods=n)*100
+    df_sub = df.loc[start_date:end_date]
+    df_sub = df_sub.loc[(df['returns']) >= percent, :]
+    # return df_sub[["returns"]].sort_index()
+    df_sub = df_sub[["returns"]].sort_index(ascending=False).to_dict()
+    final_dict = df_sub['returns']
+    a = {}
+    for i in final_dict:
+        a[str(i)] = final_dict[i]
+    return a
 
-# # certain margin around a stock  
-# @app.route('/CheckPercentChangeMargin/<ticker>/<float:stock_return>/<float:margin>') # works
-# def percent_change_margin(ticker, stock_return, margin):
-#     df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
-#         ticker, outputsize='full')[0]
-#     df["returns"] = df["5. adjusted close"].pct_change()*100  # percent change * 100
-#     df_sub = df[(df["returns"] < stock_return+margin) & (df["returns"]
-#                                                          > stock_return-margin)]  # defining returns you want
-#     # sort values by date  #sort_values(["returns"])
-#     df_sub = df_sub[["returns"]].sort_index(ascending=False).to_dict()
-#     final_dict = df_sub['returns']
-#     a = {}
-#     for i in final_dict:
-#         a[str(i)] = final_dict[i]
-#     return a
+# certain margin around a stock  
+@app.route('/CheckPercentChangeMargin/<ticker>/<float:stock_return>/<float:margin>') # works
+def percent_change_margin(ticker, stock_return, margin):
+    df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
+        ticker, outputsize='full')[0]
+    df["returns"] = df["5. adjusted close"].pct_change()*100  # percent change * 100
+    df_sub = df[(df["returns"] < stock_return+margin) & (df["returns"]
+                                                         > stock_return-margin)]  # defining returns you want
+    # sort values by date  #sort_values(["returns"])
+    df_sub = df_sub[["returns"]].sort_index(ascending=False).to_dict()
+    final_dict = df_sub['returns']
+    a = {}
+    for i in final_dict:
+        a[str(i)] = final_dict[i]
+    return a
     
 
-# # certain margin around a stock within a timeframe and period n 
-# @app.route('/CheckPercentChangeMarginTimeframePeriod/<ticker>/<int:n>/<float:stock_return>/<float:margin>/<start_date>/<end_date>') # works
-# def percent_change_margin_by_date_timeframe(ticker, n, stock_return, margin, start_date, end_date):
-#     assert pd.to_datetime(start_date) < pd.to_datetime(end_date)
-#     df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
-#         ticker, outputsize='full')[0]
-#     df = df.sort_index()
-#     df["returns"] = df["5. adjusted close"].pct_change(periods=n)*100
-#     df_sub = df.loc[start_date:end_date]
-#     df_sub = df_sub[(df["returns"] < stock_return+margin) &
-#                     (df["returns"] > stock_return-margin)]  # defining returns you want
-#     # sort values by date  #sort_values(["returns"])
-#     df_sub = df_sub[["returns"]].sort_index(ascending=False).to_dict()
-#     final_dict = df_sub['returns']
-#     a = {}
-#     for i in final_dict:
-#         a[str(i)] = final_dict[i]
-#     return a
+# certain margin around a stock within a timeframe and period n 
+@app.route('/CheckPercentChangeMarginTimeframePeriod/<ticker>/<int:n>/<float:stock_return>/<float:margin>/<start_date>/<end_date>') # works
+def percent_change_margin_by_date_timeframe(ticker, n, stock_return, margin, start_date, end_date):
+    assert pd.to_datetime(start_date) < pd.to_datetime(end_date)
+    df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
+        ticker, outputsize='full')[0]
+    df = df.sort_index()
+    df["returns"] = df["5. adjusted close"].pct_change(periods=n)*100
+    df_sub = df.loc[start_date:end_date]
+    df_sub = df_sub[(df["returns"] < stock_return+margin) &
+                    (df["returns"] > stock_return-margin)]  # defining returns you want
+    # sort values by date  #sort_values(["returns"])
+    df_sub = df_sub[["returns"]].sort_index(ascending=False).to_dict()
+    final_dict = df_sub['returns']
+    a = {}
+    for i in final_dict:
+        a[str(i)] = final_dict[i]
+    return a
 
 
-# # highest returns t within a certain timeframe over a period n 
-# @app.route('/CheckPercentTopReturns/<ticker>/<int:n>/<int:t>/<start_date>/<end_date>')
-# def top_N_returns(ticker, n, t, start_date, end_date):
-#     assert pd.to_datetime(start_date) < pd.to_datetime(end_date)
-#     df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
-#         ticker, outputsize='full')[0]
-#     df = df.sort_index()
-#     df["returns"] = df["5. adjusted close"].pct_change(periods=n)*100
-#     df_sub = df.loc[start_date:end_date]  # slice of date frame
-#     df_sub = df_sub.sort_values(["returns"], ascending=False)
-#     df_sub = df_sub[["returns"]].head(t).to_dict() # series of the returns 
-#     final_dict = df_sub['returns']
-#     a = {}
-#     for i in final_dict:
-#         a[str(i)] = final_dict[i]
-#     return a
+# highest returns t within a certain timeframe over a period n 
+@app.route('/CheckPercentTopReturns/<ticker>/<int:n>/<int:t>/<start_date>/<end_date>')
+def top_N_returns(ticker, n, t, start_date, end_date):
+    assert pd.to_datetime(start_date) < pd.to_datetime(end_date)
+    df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
+        ticker, outputsize='full')[0]
+    df = df.sort_index()
+    df["returns"] = df["5. adjusted close"].pct_change(periods=n)*100
+    df_sub = df.loc[start_date:end_date]  # slice of date frame
+    df_sub = df_sub.sort_values(["returns"], ascending=False)
+    df_sub = df_sub[["returns"]].head(t).to_dict() # series of the returns 
+    final_dict = df_sub['returns']
+    a = {}
+    for i in final_dict:
+        a[str(i)] = final_dict[i]
+    return a
 
-# # lowest returns t within a certain timeframe over a given period n 
-# @app.route('/CheckPercentBottomReturns/<ticker>/<int:n>/<int:t>/<start_date>/<end_date>')
-# def bottom_N_returns(ticker, n, t, start_date, end_date):
-#     assert pd.to_datetime(start_date) < pd.to_datetime(end_date)
-#     df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
-#         ticker, outputsize='full')[0]
-#     df = df.sort_index()
-#     df["returns"] = df["5. adjusted close"].pct_change(periods=n)*100
-#     df_sub = df.loc[start_date:end_date]  # slice of date frame
-#     df_sub = df_sub.sort_values(["returns"])
-#     df_sub = df_sub[["returns"]].head(t).to_dict()  # series of the returns
-#     final_dict = df_sub['returns']
-#     a = {}
-#     for i in final_dict:
-#         a[str(i)] = final_dict[i]
-#     return a
+# lowest returns t within a certain timeframe over a given period n 
+@app.route('/CheckPercentBottomReturns/<ticker>/<int:n>/<int:t>/<start_date>/<end_date>')
+def bottom_N_returns(ticker, n, t, start_date, end_date):
+    assert pd.to_datetime(start_date) < pd.to_datetime(end_date)
+    df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
+        ticker, outputsize='full')[0]
+    df = df.sort_index()
+    df["returns"] = df["5. adjusted close"].pct_change(periods=n)*100
+    df_sub = df.loc[start_date:end_date]  # slice of date frame
+    df_sub = df_sub.sort_values(["returns"])
+    df_sub = df_sub[["returns"]].head(t).to_dict()  # series of the returns
+    final_dict = df_sub['returns']
+    a = {}
+    for i in final_dict:
+        a[str(i)] = final_dict[i]
+    return a
 
-# # annualized returns, volatility, and sharpe over a certain timeframe
-# @app.route('/ReturnsInfo/<ticker>/<start_date>/<end_date>')
-# def return_info(ticker, start_date, end_date):
-#     assert pd.to_datetime(start_date) < pd.to_datetime(
-#         end_date), "start_date must eb less than end_date"  # make sure start date < end date
-#     df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
-#         ticker, outputsize='full')[0]  # 1st input of list
-#     df = df.sort_index()  # sort based on date
-#     df["returns"] = df["5. adjusted close"].pct_change() * 100  # get percentage change
-#     df = df.dropna(subset=["returns"])  # drop NA values
-#     df_sub = df.loc[start_date:end_date]  # slice of date frame
-#     df_sub = df_sub["returns"]  # series of the returns
-#     # annulizing number of trading days in a year. average daily return*252
-#     mean_returns = df_sub.mean()*252
-#     # volatility is standard deviation of annualized returns*square root of 252, np is numpy library
-#     volatility = df_sub.std()*np.sqrt(252)
-#     return pd.DataFrame({"Annualized Returns": [mean_returns], "Annualized Volatility": [volatility], "Annualized Sharpe": [mean_returns/volatility]}).to_dict()
+# annualized returns, volatility, and sharpe over a certain timeframe
+@app.route('/ReturnsInfo/<ticker>/<start_date>/<end_date>')
+def return_info(ticker, start_date, end_date):
+    assert pd.to_datetime(start_date) < pd.to_datetime(
+        end_date), "start_date must eb less than end_date"  # make sure start date < end date
+    df = TimeSeries(key='HY5IIUWSUZSBEEU5', output_format='pandas').get_daily_adjusted(
+        ticker, outputsize='full')[0]  # 1st input of list
+    df = df.sort_index()  # sort based on date
+    df["returns"] = df["5. adjusted close"].pct_change() * 100  # get percentage change
+    df = df.dropna(subset=["returns"])  # drop NA values
+    df_sub = df.loc[start_date:end_date]  # slice of date frame
+    df_sub = df_sub["returns"]  # series of the returns
+    # annulizing number of trading days in a year. average daily return*252
+    mean_returns = df_sub.mean()*252
+    # volatility is standard deviation of annualized returns*square root of 252, np is numpy library
+    volatility = df_sub.std()*np.sqrt(252)
+    return pd.DataFrame({"Annualized Returns": [mean_returns], "Annualized Volatility": [volatility], "Annualized Sharpe": [mean_returns/volatility]}).to_dict()
     
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=True)
